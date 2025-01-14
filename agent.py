69ea8ae82e30d84afb9bce298f903ba1381886c1
Tomas Santos <tns@cin.ufpe.br>
2025-01-14 16:59:34 -0300

    modularize the decision method

--- agent.py
+++ agent.py
@@ -5,15 +5,24 @@ class ExampleAgent(BaseAgent):
     def __init__(self, id=0, yellow=False):
         super().__init__(id, yellow)
 
-    def decision(self):
+    def manage_targets(self):
+       #Prioriza ou seleciona o próximo alvo
         if len(self.targets) == 0:
-            return
+            return None
+        return self.targets[0]
 
-        target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, self.targets[0])
+    def navigate_to_target(self, target):
+        #Navega até o alvo especificado
+        target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, target)
         self.set_vel(target_velocity)
         self.set_angle_vel(target_angle_velocity)
 
-        return
+    def decision(self):
+        target = self.manage_targets()
+        if not target:
+            return
+
+        self.navigate_to_target(target)
 
     def post_decision(self):
         pass
